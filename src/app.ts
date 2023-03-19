60eca7c78d6090c9b2eca1f3c51f157ca26b1673
derrick <iga00257@gmail.com>
2023-03-19 22:10:54 +0800

    changed url

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use('/api', router);
 const CLIENT_ID = '8388cb899ba93a909f8f';
 const CLIENT_SECRET = '1adf1feede046e53cdddd514599539afc54aeefe';
 const REDIRECT_URI = 'http://localhost:3000/callback';
-app.get('/', (req, res) => {
+app.get('/auth/github', (req, res) => {
     const state = 'SOME_STATE';
     const scope = 'user';
   
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
     res.redirect(githubAuthUrl);
   });
   
-  app.get('/callback', async (req, res) => {
+  app.get('/auth/github/callback', async (req, res) => {
     const code = req.query.code;
     const state = req.query.state;
     console.log(state)
